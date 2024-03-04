41b2910ca66b830a8e5112f985d07dc19b987568
Avishkar Kabadi <155943160+Avishkar-Kabadi@users.noreply.github.com>
2024-03-04 15:43:50 +0530

    Added New Component In

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TopheadlinesComponent } from './topheadlines/topheadlines.component';
+import { InComponent } from './in/in.component';
 
 const routes: Routes = [
-  {path:'',component:TopheadlinesComponent}
+  {path:'',component:TopheadlinesComponent},
+  {path:'in',component:InComponent}
 ];
 
 @NgModule({
